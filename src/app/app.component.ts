526a141f544bc259d144c04bf389da3bcfb1e44c
Chris Pinzaru <devpinzaru@gmail.com>
2022-01-24 20:29:02 +0200

    Filter router.events in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,8 +19,11 @@ export class AppComponent {
     public usersService: UsersService,
     private router: Router,
   ) {
-    this.router.events.subscribe((event) => {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart || event instanceof NavigationEnd)
+    ).subscribe((event) => {
       console.log(`%c${event}`, 'color: green');
+
       if (event instanceof NavigationStart) {
         this.isLoading = true;
       } else if (event instanceof NavigationEnd) {
@@ -31,39 +34,6 @@ export class AppComponent {
     setTimeout(() => {
       this.numVariable = this.usersService.num;
     }, 0)
-
-    /*
-    const obs$ = new Observable((observer: Observer<number>) => {
-      [0, 1, 2, 3, 5, 6, 7].map((number) => {
-        observer.next(number);
-      });
-    });
-    obs$.subscribe(console.log);
-
-    from([0, 1, 2, 3, 5, 6, 7]).pipe(
-      filter((number) => number % 2 === 0),
-      map((number) => number * 2),
-    ).subscribe(console.log);
-
-    from("abcde").subscribe(console.log);
-
-    from([]).pipe(
-      startWith("Please type your name")
-    ).subscribe(console.log)
-
-    of("STARTED", "FINISHED").subscribe(console.log);
-
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve("SETTIMEOUT FINISHED")
-      }, 3000);
-    })
-
-    from(promise).subscribe((value) => {
-      console.log(value);
-    })
-
-     */
   }
 
 }
